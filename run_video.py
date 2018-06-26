dc5f118a37a39c93339fb38b1e53a0ee6f1fa29e
ncantrell <38082643+ncantrell@users.noreply.github.com>
2018-06-26 04:11:37 -0700

    Update run_video.py

--- run_video.py
+++ run_video.py
@@ -2,8 +2,7 @@ import argparse
 import logging
 import time
 from IPython.display import Image, display
-
-
+from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 
@@ -47,8 +46,13 @@ if __name__ == '__main__':
         image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
 
         cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imwrite("./image.png", image)
-        display(Image('./image.png'))
+        #cv2.imwrite("./image.png", image)
+        #display(Image('./image.png'))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        plt.imshow(image)
+        plt.title('pose estimate')
+        plt.show()
+
         #cv2.imshow('tf-pose-estimation result', image)
         fps_time = time.time()
         #if cv2.waitKey(1) == 27:
