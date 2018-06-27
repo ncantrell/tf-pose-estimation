7c4405a040390f766c30e53cf2bf74a3c905b794
ncantrell <38082643+ncantrell@users.noreply.github.com>
2018-06-26 20:35:17 -0700

    Create display_vector_video.py

---
+++ display_vector_video.py
@@ -0,0 +1,72 @@
+import argparse
+import logging
+import time
+from IPython.display import Image, display
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
+
+from tf_pose.estimator import TfPoseEstimator
+from tf_pose.networks import get_graph_path, model_wh
+
+logger = logging.getLogger('TfPoseEstimator-Video')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fps_time = 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
+    parser.add_argument('--video', type=str, default='')
+    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
+    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin')
+    parser.add_argument('--show-process', type=bool, default=False,
+                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
+    args = parser.parse_args()
+
+    logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
+    w, h = model_wh(args.resolution)
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    cap = cv2.VideoCapture(args.video)
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('./video_out.avi',fourcc, 60.0, (frame_width,frame_height))
+
+    if cap.isOpened() is False:
+        print("Error opening video stream or file")
+    i = 0;
+    while cap.isOpened():
+        ret_val, image = cap.read()
+
+        humans = e.inference (image, upsample_size = 4.0)
+        if not args.showBG:
+            image = np.zeros(image.shape)
+        print(humans)
+        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+
+        cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        #cv2.imwrite("./image.png", image)
+        #print(str(image.size()))
+        out.write(image)
+        print(str(i))
+        i=i+1
+        #display(Image('./image.png'))
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #plt.imshow(image)
+        #plt.title('pose estimate')
+        #plt.show()
+
+        #cv2.imshow('tf-pose-estimation result', image)
+        fps_time = time.time()
+        #if cv2.waitKey(1) == 27:
+        #    break
+#
+    cv2.destroyAllWindows()
+#logger.debug('finished+')
